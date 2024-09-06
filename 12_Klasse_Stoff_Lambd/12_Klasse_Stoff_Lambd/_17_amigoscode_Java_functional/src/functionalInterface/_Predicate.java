12e0c43ed871b8c81cef376c2495fe493628c850
MustafaHa <mostafahasan761@gmail.com>
2024-09-06 15:29:45 +0200

    new Staff about the 12KlasseStoff

--- 12_Klasse_Stoff_Lambd/12_Klasse_Stoff_Lambd/_17_amigoscode_Java_functional/src/functionalInterface/_Predicate.java
+++ 12_Klasse_Stoff_Lambd/12_Klasse_Stoff_Lambd/_17_amigoscode_Java_functional/src/functionalInterface/_Predicate.java
@@ -1,5 +1,7 @@
 package functionalInterface;
 
+import java.util.function.Predicate;
+
 public class _Predicate {
 
 	public static void main(String[] args) {
@@ -7,6 +9,13 @@ public class _Predicate {
 		
 		System.out.println(isPhoneNumberValid("07000000000"));
 		System.out.println(isPhoneNumberValid("09000896000"));
+		
+		
+		System.out.println("With Predicate");
+		System.out.println(isPhoneNumberValidPredicate.test("07000000000"));
+		System.out.println(isPhoneNumberValidPredicate.test("09000896000"));
+
+	
 
 	}
 	
@@ -14,4 +23,9 @@ public class _Predicate {
 		return phoneNumber.startsWith("07") && phoneNumber.length() == 11;
 	}
 
+	static Predicate<String> isPhoneNumberValidPredicate = phoneNumber -> {
+		return phoneNumber.startsWith("07") && phoneNumber.length() == 11;
+
+	};
+	
 }
