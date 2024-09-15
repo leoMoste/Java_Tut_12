a63e10876dc3df717e7125ed4225bdd3eed525bf
MustafaHa <mostafahasan761@gmail.com>
2024-09-15 16:39:47 +0200

    Add a new update of the merge sort by BroCode YT

--- 12_Klasse_Stoff_Lambd/12_Klasse_Stoff_Lambd/MergeSort/src/MergeMain.java
+++ 12_Klasse_Stoff_Lambd/12_Klasse_Stoff_Lambd/MergeSort/src/MergeMain.java
@@ -5,79 +5,93 @@ public class MergeMain {
 	// Divide-and-conquer => Merge
 	public static void main(String[] args) {
 		
-		    int[] arr = { 60, 10, 20, 5, 60, 70 };
-	        int n = arr.length;
+		    int[] Mainarray = { 3, 7, 8, 5, 4 , 2, 6, 1};
+	        int n = Mainarray.length;
 
 	        System.out.println("Unsorted array:");
-	        printArray(arr);
+	        printArray(Mainarray);
 
-	        mergeSort(arr, 0, n - 1);
+	        mergeSort(Mainarray);
 
 	        System.out.println("Sorted array:");
-	        printArray(arr);
+	        printArray(Mainarray);
 
 	}
 	
-    // Function to merge two subarrays of arr[]
+    private static void mergeSort(int[] mainArray) {
+		
+    	int length = mainArray.length;
+    	
+    	if(length <= 1) {
+    		return; // base case
+    	}
+    	
+    	int middle = length/2;  // 8/2 = 4
+    	
+    	// now create two new Sub-Array 
+    	int[] leftArray = new int[middle]; // int[4] 
+    	int[] rightArray = new int[length -  middle];  //  int[8 - 4] => int[4]
+    	
+    	// now we need to copy the elements of our 
+    	// original mainArray to our left and right
+    	int left = 0; // index for left array
+    	int right = 0; // index for right array
+    	// and like this we fill the two sub-Arrays
+    	for(; left < length; left++) {
+    		if(left < middle) {
+    			leftArray[left] = mainArray[left];
+    		}
+    		else {
+    			rightArray[right] = mainArray[left];
+    			right++;
+    		}
+    	}
+    	
+    	mergeSort(leftArray);
+    	mergeSort(rightArray);
+    	merge(leftArray, rightArray, mainArray);
+		
+	}
+
+	// Function to merge two subarrays of arr[]
     // First subarray is arr[l..m]
     // Second subarray is arr[m+1..r]
-    public static void merge(int arr[], int l, int m, int r) {
-        int n1 = m - l + 1; // Size of the first subarray
-        int n2 = r - m;     // Size of the second subarray
-
-        // Temporary arrays to hold the elements
-        int[] L = new int[n1];
-        int[] R = new int[n2];
-
-        // Copy data to temp arrays L[] and R[]
-        for (int i = 0; i < n1; ++i)
-            L[i] = arr[l + i];
-        for (int j = 0; j < n2; ++j)
-            R[j] = arr[m + 1 + j];
-
-        // Merge the temporary arrays back into arr[l..r]
-        int i = 0, j = 0;
-        int k = l; // Left Initial index of the merged subarray
-
-        while (i < n1 && j < n2) {
-            if (L[i] <= R[j]) {
-                arr[k] = L[i];
-                i++;
-            } else {
-                arr[k] = R[j];
-                j++;
-            }
-            k++;
-        }
-
-        // Copy the remaining elements of L[], if any
-        while (i < n1) {
-            arr[k] = L[i];
-            i++;
-            k++;
-        }
-
-        // Copy the remaining elements of R[], if any
-        while (j < n2) {
-            arr[k] = R[j];
-            j++;
-            k++;
-        }
-    }
-
-    // Function to implement merge sort
-    public static void mergeSort(int arr[], int l, int r) {
-        if (l < r) {
-            int m = l + (r - l) / 2; // Find the middle point
-
-            // Recursively sort first and second halves
-            mergeSort(arr, l, m);
-            mergeSort(arr, m + 1, r);
-
-            // Merge the sorted halves
-            merge(arr, l, m, r);
-        }
-    }
+    private static void merge(int[] leftArray, int[] rightArray, int[] mainArray) {
+		int leftSize = mainArray.length / 2;
+		int rightSize = mainArray.length - leftSize;
+		int index = 0, left = 0, right = 0; // indecies
+		
+		// check the condtions for merging
+		while (index < leftSize && right < rightSize) {
+			if(leftArray[left] < rightArray[right]) {
+				mainArray[index] = leftArray[left];
+				index++;
+				left++;
+			}
+			else {
+				mainArray[index] = rightArray[right];
+				index++;
+				right++;
+			}
+		}
+		
+		
+		// there is going to be one element remaining that we cannot compare
+		// to another element becuase there is only one left
+		while (left < leftSize) {
+			mainArray[index] = leftArray[left];
+			index++;
+			left++;
+		}
+		
+		while (right < rightSize) {
+			mainArray[index] = rightArray[right];
+			index++;
+			right++;
+		}
+		
+	}
+  
 
     // Function to print the array
     public static void printArray(int arr[]) {
